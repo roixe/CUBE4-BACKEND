69ea98c56c51887aec3e87af97ff6fe61e422ac6
hanan3889 <rebaia_hanan@yahoo.fr>
2025-02-21 13:50:08 +0100

    add articleDTO

---
+++ JamaisASec/JamaisASec/Models/ArticleDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace JamaisASec.Models
+{
+    public class ArticleDTO
+    {
+        public int id { get; set; }
+        public string? nom { get; set; }
+        public string? description { get; set; }
+        public int quantite { get; set; }
+        public int quantite_Min { get; set; }
+        public int colisage { get; set; }
+        public int prix_unitaire { get; set; }
+        public int annee { get; set; }
+        public Famille? famille { get; set; }
+        public Maison? maison { get; set; }
+        public Fournisseur? fournisseur { get; set; }
+        public bool IsSelected { get; internal set; }
+      
+    }
+}
